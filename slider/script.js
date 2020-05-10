84aacc8c940644f3255a46b17da39e2992cc8f5d
Doctor79 <51418398+Doctor79@users.noreply.github.com>
2020-05-10 23:50:15 +0100

    Add files via upload

--- slider/script.js
+++ slider/script.js
@@ -1,50 +1,42 @@
-const leftButton = document.querySelector(".left");
-const rightButton = document.querySelector('.right');
+const rightButton = document.querySelector('.right')
+const leftButton = document.querySelector(".left")
 const frameNumber = document.querySelector('.number p')
 let index = 0
+const img = document.createElement('img')
+const kontener = document.querySelector('.kontener')
 
 const imagesArr = [
     './images/1.jpg',
     './images/2.jpg',
     './images/3.jpg',
     './images/4.jpg',
-    './images/5.jpg'
+    './images/5.jpg',
+    'https://img.joemonster.org/mg/albums/052020/main_10sztuka_medytacji_ny_raul28.jpg'
 ]
-
-const img = document.createElement('img')
-const kontener = document.querySelector('.kontener')
+frameNumber.textContent = index + 1 + "/" + imagesArr.length;
 img.setAttribute('src', imagesArr[index])
 kontener.appendChild(img)
-leftButton.addEventListener("click", left);
-rightButton.addEventListener("click", right);
-frameNumber.textContent = index + 1 + '/5';
 
-
-function left() {
-
+leftButton.addEventListener("click", function() {
     if (index <= imagesArr.length) {
         rightButton.classList.remove('disable')
         index--
-        // let number = index;
         img.setAttribute('src', imagesArr[index])
-
-        frameNumber.textContent = index + 1 + '/5';
+        frameNumber.textContent = index + 1 + "/" + imagesArr.length;
     }
     if (index === 0) {
         leftButton.classList.add('disable')
     }
-}
-
-function right() {
+});
+rightButton.addEventListener("click", function() {
     if (index >= 0) {
         leftButton.classList.remove('disable')
         index++
-
         img.setAttribute('src', imagesArr[index])
-        frameNumber.textContent = index + 1 + '/5';
+        frameNumber.textContent = index + 1 + "/" + imagesArr.length;
         if (index === imagesArr.length - 1) {
             rightButton.classList.add('disable')
         }
     }
 
-}
+});
