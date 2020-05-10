b440ba127cb36acf2b20b53f6de2f7cd4dd5ff46
Doctor79 <51418398+Doctor79@users.noreply.github.com>
2020-05-10 21:49:07 +0100

    Add files via upload

---
+++ slider/a.js
@@ -0,0 +1,12 @@
+const leftButton = document.querySelector(".left");
+const rightButton = document.querySelector('.right');
+const frameNumber = document.querySelector('.number p');
+let index = 0
+const imagesArr = ['./images/1.jpg', './images/2.jpg', './images/3.jpg', './images/4.jpg', './images/5.jpg']
+const img = document.createElement('img') const kontener = document.querySelector('.kontener') img.setAttribute('src', imagesArr[index]) kontener.appendChild(img) leftButton.addEventListener("click", left);
+rightButton.addEventListener("click", right);
+frameNumber.textContent = index + 1 + '/5';
+
+function left() {
+    if (index <= i magesArr.length) {
+        rightButton.classList.remove('disable') index-- // let number=i ndex; img.setAttribute( 'src', imagesArr[index]) frameNumber.textContent=i ndex + 1 + '/5'; } if (index===0 ) { leftButton.classList.add( 'disable') } } function right() { if (index>= 0) { leftButton.classList.remove('disable') index++ img.setAttribute('src', imagesArr[index]) frameNumber.textContent = index + 1 + '/5'; if (index === imagesArr.length - 1) { rightButton.classList.add('disable') } } }
