96092f27f41e582d7db4e73672189272e86a500a
AdamG5161 <58313638+AdamG5161@users.noreply.github.com>
2020-05-12 11:40:32 +0100

    update
    Nie wiem czy sie beda obrazy z galerii ladowac, sprawdz. Html tez poszedl nowy, sprawdz.

--- slider/script.js
+++ slider/script.js
@@ -1,41 +1,36 @@
-const rightButton = document.querySelector('.right')
-const leftButton = document.querySelector(".left")
-const frameNumber = document.querySelector('.number p')
-let index = 0
-const img = document.createElement('img')
-const kontener = document.querySelector('.kontener')
+const rightButton = document.querySelector('.right');
+const leftButton = document.querySelector(".left");
+const frameNumber = document.querySelector('.number p');
+let index = 1;
+const img = document.createElement('img');
+const kontener = document.querySelector('.kontener');
 
-const imagesArr = [
-    './images/1.jpg',
-    './images/2.jpg',
-    './images/3.jpg',
-    './images/4.jpg',
-    './images/5.jpg',
-    'https://img.joemonster.org/mg/albums/052020/main_10sztuka_medytacji_ny_raul28.jpg'
-]
-frameNumber.textContent = index + 1 + "/" + imagesArr.length;
-img.setAttribute('src', imagesArr[index])
-kontener.appendChild(img)
+ 
+frameNumber.textContent = index+ "/5";
+img.setAttribute('src', `images/1.jpg`);
+kontener.appendChild(img);
 
 leftButton.addEventListener("click", function() {
-    if (index <= imagesArr.length) {
-        rightButton.classList.remove('disable')
-        index--
-        img.setAttribute('src', imagesArr[index])
-        frameNumber.textContent = index + 1 + "/" + imagesArr.length;
+    if (index <= 5) {
+        rightButton.classList.remove('disable');
+        index--;
+        img.setAttribute('src', `images/${index}.jpg`);
+        frameNumber.textContent = index+ "/5";
     }
-    if (index === 0) {
-        leftButton.classList.add('disable')
+    if (index === 1) {
+        leftButton.classList.add('disable');
+        img.setAttribute('src', `images/1.jpg`);
     }
 });
 rightButton.addEventListener("click", function() {
     if (index >= 0) {
-        leftButton.classList.remove('disable')
-        index++
-        img.setAttribute('src', imagesArr[index])
-        frameNumber.textContent = index + 1 + "/" + imagesArr.length;
-        if (index === imagesArr.length - 1) {
-            rightButton.classList.add('disable')
+        leftButton.classList.remove('disable');
+        index++;
+        img.setAttribute('src', `images/${index}.jpg`);
+        frameNumber.textContent = index+ "/5";
+        if (index === 5) {
+            rightButton.classList.add('disable');
+            img.setAttribute('src', `images/5.jpg`);
         }
     }
 
